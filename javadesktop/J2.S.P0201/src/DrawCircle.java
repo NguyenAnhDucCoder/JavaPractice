27585a3d8ca8d8b83e573227149ccf6e8decf047
NguyenAnhDuc <soitrangtn123@gmail.com>
2020-03-27 12:07:17 +0700

    commit

--- javadesktop/J2.S.P0201/src/DrawCircle.java
+++ javadesktop/J2.S.P0201/src/DrawCircle.java
@@ -19,16 +19,22 @@ public class DrawCircle extends JPanel {
 
     public DrawCircle(MainScreen mainScreen) {
         this.mainScreen = mainScreen;
+        // setBackground for JPanel 
         this.setBackground(Color.WHITE);
-        this.setBounds(mainScreen.getjPanel_Screen().getWidth()/2 - 25, mainScreen.getjPanel_Screen().getHeight()/2 - 25, 50, 50);
+        // set width height X Y for jpanel
+        this.setBounds(mainScreen.getjPanel_Screen().getWidth() / 2 - 25, mainScreen.getjPanel_Screen().getHeight() / 2 - 25, 50, 50);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
-        g.setColor(Color.BLUE);
+        // draw circle
         g.drawOval(0, 0, 50, 50);
+        // set color for jpanel
+        g.setColor(Color.BLUE);
+        // fill circle
         g.fillOval(0, 0, 50, 50);
+        
     }
 
 }
--- javadesktop/J2.S.P0201/src/MainScreenController.java
+++ javadesktop/J2.S.P0201/src/MainScreenController.java
@@ -23,50 +23,61 @@ public class MainScreenController {
     public MainScreenController() {
         mainScreen = new MainScreen();
         DrawCircle circle = new DrawCircle(mainScreen);
+        // add circle panel into jPanel_Screen
         mainScreen.getjPanel_Screen().add(circle);
         mainScreen.getBtnBottom().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
+                // check if cirle panel over bottom
                 if (mainScreen.getjPanel_Screen().getHeight()
                         < circle.getY() + circle.getHeight() + move) {
-                    JOptionPane.showMessageDialog(null, "Cannot move anymore !!!", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Cannot move anymore !!!", "Warning", JOptionPane.WARNING_MESSAGE);
                     return;
                 }
+                // set position Y + 10
                 circle.setLocation(circle.getX(), circle.getY() + move);
             }
         });
         mainScreen.getBtnLeft().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
+                // check if cirle panel over left
                 if (0 > circle.getX() - move) {
-                    JOptionPane.showMessageDialog(null, "Cannot move anymore !!!", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Cannot move anymore !!!", "Warning", JOptionPane.WARNING_MESSAGE);
                     return;
                 }
+                // set position X - 10
                 circle.setLocation(circle.getX() - move, circle.getY());
             }
         });
         mainScreen.getBtnRight().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
+                // check if cirle panel over Right
                 if (mainScreen.getjPanel_Screen().getWidth()
-                        < circle.getX() + move +circle.getWidth()) {
-                    JOptionPane.showMessageDialog(null, "Cannot move anymore !!!", "Error", JOptionPane.ERROR_MESSAGE);
+                        < circle.getX() + move + circle.getWidth()) {
+                    JOptionPane.showMessageDialog(null, "Cannot move anymore !!!", "Warning", JOptionPane.WARNING_MESSAGE);
                     return;
                 }
-               circle.setLocation(circle.getX() + move, circle.getY());
+                // set position X + 10
+                circle.setLocation(circle.getX() + move, circle.getY());
             }
         });
         mainScreen.getBtnUp().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
+                // check if cirle panel over Up
                 if (0 > circle.getY() - move) {
-                    JOptionPane.showMessageDialog(null, "Cannot move anymore !!!", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Cannot move anymore !!!", "Warning", JOptionPane.WARNING_MESSAGE);
                     return;
                 }
-                circle.setLocation(circle.getX(),circle.getY() - move);
+                // set position Y - 10
+                circle.setLocation(circle.getX(), circle.getY() - move);
             }
         });
+        // set center screen
         mainScreen.setLocationRelativeTo(null);
+        // set visible screen
         mainScreen.setVisible(true);
     }
 }
